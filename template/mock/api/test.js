f44d828100b81a825cf79728bc5833b1635c2f0c
longxiang.wang <longxiang.wang@vipshop.com>
2019-02-09 20:03:47 +0800

    为template添加mock工具和相关配置

---
+++ template/mock/api/test.js
@@ -0,0 +1,5 @@
+module.exports = {
+  code: 200,
+  msg: 'success',
+  data: 'test api'
+}
---
+++ template/mock/map.js
@@ -0,0 +1,5 @@
+const path = require('path')
+
+module.exports = [
+  // ['/request/path', '/mock/file/path']
+]
