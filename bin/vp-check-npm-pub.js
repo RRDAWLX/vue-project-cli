447232d88eaf865b157af594fcec658cb53a1972
longxiang.wang <longxiang.wang@vipshop.com>
2020-07-12 18:11:45 +0800

    check-npm-pub中增加对README.md是否已更新的检查。

--- bin/vp-check-npm-pub.js
+++ bin/vp-check-npm-pub.js
@@ -2,17 +2,30 @@
 
 /**
  * 在 npm publish 执行前做以下检查工作：
- * 1、当前分支是否为 master。
- * 2、是否有未提交的修改。
- * 3、本地仓库是否与远端保持一致。
+ * 1、README是否已更新。
+ * 2、当前分支是否为 master。
+ * 3、是否有未提交的修改。
+ * 4、本地仓库是否与远端保持一致。
  */
 
 const execSync = require('child_process').execSync
 const chalk = require('chalk')
+const readlineSync = require('readline-sync')
 const execOpts = {
   encoding: 'utf8',
 }
 
+// README是否已更新
+let readmeUpdated = readlineSync.keyInYN('README.md 是否已更新？')
+readmeUpdated = readmeUpdated === true
+
+if (readmeUpdated) {
+  console.log(chalk.green('✔ README.md 已更新'))
+} else {
+  console.log(chalk.red('✘ README.md 未更新'))
+}
+
+// 当前分支是否为 master
 const masterRegexp = /\bmaster\b/
 let branchInfo = execSync('git branch', execOpts)
 let isMaster = masterRegexp.test(branchInfo)
@@ -23,6 +36,7 @@ if (isMaster) {
   console.log(chalk.red('✘ 当前分支非 master'))
 }
 
+// 是否有未提交的修改
 let statusInfo = execSync('git status --porcelain', execOpts)
 let clean = statusInfo.length === 0
 
@@ -32,6 +46,7 @@ if (clean) {
   console.log(chalk.red('✘ 有未提交修改'))
 }
 
+// 本地仓库是否与远端保持一致
 try {
   execSync('git fetch')
 } catch (e) {
@@ -47,7 +62,7 @@ if (upToDate) {
   console.log(chalk.red('✘ 本地仓库未与远端仓库同步'))
 }
 
-if (isMaster && clean && upToDate) {
+if (readmeUpdated && isMaster && clean && upToDate) {
   process.exit(0)
 } else {
   process.exit(1)
